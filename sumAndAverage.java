b129f548f85795d9f92089b2549306f48591e355
kishankumartiwar <tiwarikishan297@gmail.com>
2024-12-22 22:52:37 +0530

    sum and average

---
+++ sumAndAverage.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class sumAndAverage {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("please enter the number of elements");
+    int size = sc.nextInt();
+    int[] arr = new int[size];
+    System.out.println("please enter the numbers");
+    for (int i = 0; i < size; i++) {
+      arr[i] = sc.nextInt();
+    }
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+      sum += arr[i];
+    }
+    System.out.println("sum of the given numbers is " + sum);
+    int average = sum / size;
+    System.out.println("average of the given numbers is " + average);
+  }
+}
