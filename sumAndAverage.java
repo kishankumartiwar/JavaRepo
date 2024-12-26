cae91e28b5630489de922a5a6486c926502acf71
kishankumartiwar <tiwarikishan297@gmail.com>
2024-12-26 20:35:43 +0530

    commit

--- sumAndAverage.java
+++ sumAndAverage.java
@@ -1,4 +1,4 @@
-/* import java.util.Scanner;
+import java.util.Scanner;
 
 public class sumAndAverage {
   public static void main(String[] args) {
@@ -19,4 +19,3 @@ public class sumAndAverage {
     System.out.println("average of the given numbers is " + average);
   }
 }
- */
